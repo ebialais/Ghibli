6aa08c768eec943fc1c00a826eabd36580f60d23
Emilie Bialais <ebialais@gmail.com>
2020-07-27 11:13:10 +0200

    axios call

---
+++ src/Components/FilmList.jsx
@@ -0,0 +1,53 @@
+import React from 'react';
+import axios from 'axios'; 
+
+export default class FilmList extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            films: [],
+            error: null, 
+            isLoading: false
+        };
+    };
+    componentDidMount() {
+        axios.get(`https://ghibliapi.herokuapp.com/films/`)
+        .then(
+            res => {
+                const films = res.data;
+                this.setState({ 
+                    isLoading: true,
+                    films: films
+                });
+            },
+            (error) => {
+                this.setState({
+                    isLoaded: true,
+                    error
+                });
+            }
+        )
+    }
+
+    render () {
+        const { error, isLoading, films } = this.state;
+        if ( error ) {
+            return <h1>Erreur</h1>
+        } else if ( !isLoading ) {
+            return <h1>Is Loading</h1>
+        } else {
+            return (
+                <div>
+                    <h1>Les films</h1>
+                    <ul>
+                        { films.map((film) => 
+                            <li key={film.id}>
+                                {film.title}
+                            </li>
+                        )}
+                    </ul>
+                </div>
+            )
+        }
+    }
+}
